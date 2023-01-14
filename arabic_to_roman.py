9eca09237055114373b1ae82558952ad9f214512
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-14 17:30:07 +0200

    Add the conversion of the number two

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,3 +1,5 @@
 class ArabicRomanConverter():
     def convert(self, number: int) -> str:
+        if number == 2:
+            return "II"
         return "I"
--- arabic_to_roman_test.py
+++ arabic_to_roman_test.py
@@ -3,6 +3,10 @@ from unittest import TestCase
 from arabic_to_roman import ArabicRomanConverter
 
 class TestArabicRomanConverter(TestCase):
-    def test_number_one_to_I(self):
+    def test_number_one(self):
         converter = ArabicRomanConverter()
         self.assertEqual(converter.convert(1), "I")
+    
+    def test_number_two(self):
+        converter = ArabicRomanConverter()
+        self.assertEqual(converter.convert(2), "II")
