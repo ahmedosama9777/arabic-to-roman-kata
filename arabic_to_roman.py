139d78f590b2075f08b8042bb6a6af1ef1bb63da
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-14 18:30:40 +0200

    Add conversion up to 4999

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,15 +1,13 @@
 class ArabicRomanConverter():
+    arabicDigits = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    romanDigits = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+
     def convert(self, arabic: int) -> str:
         roman: str = "" 
-
-        if arabic == 10:
-            return "X"
-
-        while arabic >= 5:
-            roman += "V"
-            arabic -= 5
-
-        for i in range(arabic):
-            roman += "I"
+        
+        for i in range(len(self.arabicDigits) - 1, -1, -1):
+            while arabic >= self.arabicDigits[i]:
+                roman += self.romanDigits[i]
+                arabic -= self.arabicDigits[i]
         
         return roman
--- arabic_to_roman_test.py
+++ arabic_to_roman_test.py
@@ -15,11 +15,32 @@ class TestArabicRomanConverter(TestCase):
     def test_number_three(self):
         self.assertEqual(self.converter.convert(3), "III")
     
+    def test_number_four(self):
+        self.assertEqual(self.converter.convert(4), "IV")
+    
     def test_number_five(self):
         self.assertEqual(self.converter.convert(5), "V")
     
     def test_number_seven(self):
         self.assertEqual(self.converter.convert(7), "VII")
     
+    def test_number_nine(self):
+        self.assertEqual(self.converter.convert(9), "IX")
+    
     def test_number_ten(self):
         self.assertEqual(self.converter.convert(10), "X")
+    
+    def test_number_fifteen(self):
+        self.assertEqual(self.converter.convert(16), "XVI")
+    
+    def test_number_twentyone(self):
+        self.assertEqual(self.converter.convert(21), "XXI")
+
+    def test_number_fiftyone(self):
+        self.assertEqual(self.converter.convert(51), "LI")
+    
+    def test_number_fourthousand_nine_hundred_ninety_nine(self):
+        self.assertEqual(self.converter.convert(4999), "MMMMCMXCIX")
+    
+    
+    
